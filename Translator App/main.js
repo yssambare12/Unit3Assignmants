682c4da5b1fd1737c9fb804e6c24d26fac847f3d
Yogesh  Sambare <bloggingscan@gmail.com>
2021-11-02 15:33:09 +0530

    Translator App

---
+++ Translator App/main.js
@@ -0,0 +1,63 @@
+
+var button = document.getElementById('translate');
+button.addEventListener("click", translate);
+
+
+async function translate() {
+    var input = document.getElementById('input').value;
+
+    function input_lan() {
+        var input_language = document.getElementById('input_language').value;
+        return input_language;
+    }
+    function output_lan() {
+        var output_language = document.getElementById('output_language').value;
+        return output_language;
+    }
+    var inp = input_lan();
+    var out = output_lan();
+
+    const res = await fetch("https://libretranslate.de/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: input,
+            source: inp,
+            target: out,
+        }),
+        headers: { "Content-Type": "application/json" },
+    })
+        .then((res) => {
+            var result = res.json()
+                .then((data) => {
+                    var output = document.getElementById('output');
+                    if (data.translatedText == undefined) {
+                        output.innerText = '';
+                    } else {
+                        output.innerText = data.translatedText;
+                    }
+                })
+        }).catch((err) => {
+        })
+
+}
+
+
+
+function copytext() {
+    let text = document.getElementById('output');
+    text.select();
+    navigator.clipboard.writeText(text.value);
+}
+
+var voice_button = document.getElementById('voice_button');
+voice_button.addEventListener("click",voice);
+
+function voice() {
+    var recoginition = new webkitSpeechRecognition();
+    recoginition.lang = "en-GB";
+    recoginition.onresult = function (event) {
+        let input = document.getElementById('input');
+        input.innerText = event.results[0][0].transcript;
+    }
+    recoginition.start();
+}
