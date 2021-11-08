2f625e2cb0cb53039b04c7fd261d3ceee53c225a
Yogesh  Sambare <bloggingscan@gmail.com>
2021-11-08 16:54:41 +0530

    Evolution

---
+++ Evolution 8th November/components/search.js
@@ -0,0 +1,8 @@
+function search(){
+    return `<div>
+    <div><input type="text" id="searchinput" placeholder="Search Your Food">
+    <button id="searchbtn">search</button></div>
+    </div>`
+}
+
+export default search
